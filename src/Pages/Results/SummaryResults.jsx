9da060b3704441d249f6e5c58306fc114e38d8b2
EpifaniosEbos <EpifaniosC@ebos.com.cy>
2024-08-29 12:27:51 +0300

    fetch data from API in Summary Results Component

--- src/Pages/Results/SummaryResults.jsx
+++ src/Pages/Results/SummaryResults.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 function SummaryResults() {
     const location = useLocation();
-    const { data } = location.state || {};  // Retrieve the data passed from the previous component
+    const { solutionData } = location.state || {};  // Retrieve the data passed from the previous component
+    const [sectorServiceData, setSectorServiceData] = useState(null);
+    const [solutionAnalysisData, setSolutionAnalysisData] = useState([]);
+    const [filteredSolutionAnalysisData, setFilteredSolutionAnalysisData] = useState({});
 
-    console.log('Received data:', data);
+    useEffect(() => {
+        try {
+            const storedSectorServiceData = JSON.parse(localStorage.getItem('sectorsServicesDetails'));
+            const storedSolutionAnalysisData = JSON.parse(localStorage.getItem('solutionsAnalysisResponse'));
+
+            setSectorServiceData(storedSectorServiceData);
+            setSolutionAnalysisData(storedSolutionAnalysisData);
+        } catch (error) {
+            console.error('Error parsing localStorage data:', error);
+        }
+    }, []);
+
+    useEffect(() => {
+        console.log('Solution data:', solutionData);
+        console.log('Sector Service Data:', sectorServiceData);
+        console.log('Solution Analysis data:', solutionAnalysisData);
+        console.log('Filtered data:', filteredSolutionAnalysisData);
+
+        const filteredData = solutionAnalysisData?.analysisResults?.find(item => item.id === solutionData?.Sol_ID.toString());
+        setFilteredSolutionAnalysisData(filteredData);
+    }, [solutionData, sectorServiceData, solutionAnalysisData, filteredSolutionAnalysisData]);
 
     return(
         <h1>Summary Results</h1>
