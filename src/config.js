5937e1a091f7286884a0d3199ca4e4d86167fccc
EpifaniosEbos <EpifaniosC@ebos.com.cy>
2024-11-07 15:37:20 +0200

    For debugging config.js

--- src/config.js
+++ src/config.js
@@ -2,4 +2,8 @@ const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 const API3_BASE_URL = process.env.REACT_APP_API3_BASE_URL;
 const API1_BASE_URL = process.env.REACT_APP_API1_BASE_URL;
 
+console.log("API_BASE_URL:", API_BASE_URL); // For debugging
+console.log("API3_BASE_URL:", API3_BASE_URL);
+console.log("API1_BASE_URL:", API1_BASE_URL);
+
 export { API_BASE_URL, API3_BASE_URL, API1_BASE_URL };
