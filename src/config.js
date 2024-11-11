50dc23caf33389487938d5c4738f70af11202922
EpifaniosEbos <EpifaniosC@ebos.com.cy>
2024-11-11 08:47:44 +0200

    update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const API_BASE_URL = 'http://xgain.iccs.gr:8000';
 const API3_BASE_URL = process.env.REACT_APP_API3_BASE_URL;
 const API1_BASE_URL = process.env.REACT_APP_API1_BASE_URL;
 
-console.log("API_BASE_URL link:", API_BASE_URL); // For debugging
-console.log("API3_BASE_URL link:", API3_BASE_URL);
-console.log("API1_BASE_URL link:", API1_BASE_URL);
+console.log("API_BASE_URL:", API_BASE_URL); // For debugging
+console.log("API3_BASE_URL:", API3_BASE_URL);
+console.log("API1_BASE_URL:", API1_BASE_URL);
 
 export { API_BASE_URL, API3_BASE_URL, API1_BASE_URL };
